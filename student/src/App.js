0993842a290c07162cd7532c99e3bd748b88690c
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2022-05-22 14:12:44 +0500

    front end added

---
+++ student/src/App.js
@@ -0,0 +1,31 @@
+
+// THIS IS THE PAGE WHICH IS COMBING ALL THE COMPONENTS OF THE API
+import { Routes, Route } from "react-router-dom";
+import Axios from "axios";
+import "./App.css";
+// HERE I IMPORTED BOOTSTRAP FROM NODE MODULES BECAUSE I INSTALLED BOOTSTRAP
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// ALWAYS START THE NAME OF THE CUSTOM FILE YOU ARE IMPORTING WITH CAPITAL LETTER
+import {Head} from "./components/Head";
+import {Home} from "./components/Home";
+import {Add} from "./components/Add";
+import {Update} from "./components/Update";
+import {View} from "./components/View";
+
+
+function App() {
+  return (
+    <>
+      <Head />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/update/:id" element={<Update />} />
+        <Route path="/view/:id" element={<View />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
