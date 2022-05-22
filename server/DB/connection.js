50b3ccd49c0ca28e8290c29ee96c3b3b0208a904
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2022-05-22 21:41:43 +0500

    .

--- server/DB/connection.js
+++ server/DB/connection.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-// dotenv.config({ path: "config.env" });
+
 // databse link
-// const DB =
-//   "mongodb+srv://mateen:mughal470@cluster0.w4pbb.mongodb.net/crud?retryWrites=true&w=majority";
-dotenv.config();
-mongoose.connect(process.env.DB,{
+const DB =
+  "mongodb+srv://mateen:mughal470@cluster0.w4pbb.mongodb.net/crud?retryWrites=true&w=majority";
+
+mongoose.connect(DB,{
 useNewUrlParser:true,
 useUnifiedTopology:true
 }).then(()=>{console.log('MONGODB connected successfully')}).catch((error)=>{console.log(error.message)})
